8019ca7027e8849de485ad3628f72441461588a5
Daniel Wahl <32069932+DaniWahl@users.noreply.github.com>
2018-03-04 21:04:56 +0100

    adding initial files and assets for main UI

--- main.js
+++ main.js
@@ -6,6 +6,29 @@ const {app, BrowserWindow, Menu, ipcMain} = electron
 
 let mainWindow
 
-app.on('ready', function() {
+
+
+// start application when ready
+app.on('ready', app_init)
+
+
+
+
+
+
+/**
+ * application startup handler
+ */
+function app_init() {
     mainWindow = new BrowserWindow()
-})
+    mainWindow.loadURL(`file://${__dirname}/ui/mainWindow.html`)
+
+    mainWindow.on('closed', app_quit)
+}
+
+/**
+ * application shutdown handler
+ */
+function app_quit() {
+    app.quit()
+}
