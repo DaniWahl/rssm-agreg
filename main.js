ddf6b4d3290412f2a9daf0673a6759104ce0f5c0
Daniel Wahl <32069932+DaniWahl@users.noreply.github.com>
2018-03-06 18:42:15 +0100

    organized some ui code and extended app menu

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const electron = require('electron')
 const path = require('path')
+const mainMenuTemplate = require(__dirname + '/ui/main/mainAppMenu').mainMenuTemplate
 
 
 const {app, BrowserWindow, Menu, ipcMain} = electron
@@ -21,7 +22,7 @@ app.on('ready', app_init)
  */
 function app_init() {
     mainWindow = new BrowserWindow()
-    mainWindow.loadURL(`file://${__dirname}/ui/mainWindow.html`)
+    mainWindow.loadURL(`file://${__dirname}/ui/main/mainWindow.html`)
 
     mainWindow.on('closed', app_quit)
 
@@ -37,70 +38,3 @@ function app_quit() {
     app.quit()
 }
 
-
-// create menu template
-const mainMenuTemplate = [
-    {
-        label : 'AG RSSM'
-    },{
-        label : 'Aktienregister',
-        submenu: [
-            {
-                label: 'Aktionäre Aktuell',
-                click() {  }
-            },
-            {
-                label: 'Aktionäre Historie ',
-                click() {  }
-            },
-            {
-                label: 'Aktien',
-                click() {  }
-            },{
-                label: 'Journal',
-                click() {}
-            }
-        ]
-    },
-    {
-        label : 'Aktionen',
-        submenu: [
-            {
-                label: 'Kauf',
-                click() {  }
-            },
-            {
-                label: 'Rückkauf',
-                click() {  }
-            },
-            {
-                label: 'Übertrag',
-                click() {  }
-            },
-            {
-                label: 'Mutation',
-                click() {  }
-            }
-        ]
-    }
-]
-// handle macs first menu item
-if(process.platform !== 'darwin') {
-    // add an empty menu first
-   // mainMenuTemplate.unshift({});
-}
-// add developper tools if not in production
-if(process.env.NODE_ENV !== 'production') {
-    mainMenuTemplate.push({
-        label : 'Tools',
-        submenu: [{
-            label : 'Toggle DevTools',
-            accelerator: process.platform == 'darwin' ? 'Command+I' : 'Control+I',
-            click(item, focusedWindiow) {
-                focusedWindiow.toggleDevTools()
-            }
-        },{
-            role: 'reload'
-        }]
-    })
-}
